b6a2bbe71f0e317cc3805be1b4b88a14a685dbb8
Luca Raffo <raffoluca18@gmail.com>
2026-04-02 13:41:44 +0200

    .

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .kernels import GaussianKernel
 from .features import FunctionValues
 from .gaussian_ot import GaussianOT
-from .lifted_si import LiftedSI
+from .embedded_interpolants import EmbeddedInterpolants
 from .evaluation import sliced_wasserstein1, summary_stats
 from . import distributions
